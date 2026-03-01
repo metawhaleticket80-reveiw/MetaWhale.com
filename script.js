acc6dbefdcb68854006ccce5582bf246b8c59f03
metawhaleticket80-reveiw <metawhaleticket80@gmail.com>
2026-03-01 17:25:15 +0530

    Add menu and disclaimer control functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function openMenu(){
+document.getElementById("menuPage").style.right="0"
+}
+
+function closeMenu(){
+document.getElementById("menuPage").style.right="-100%"
+}
+
+function openDisclaimer(){
+document.getElementById("popup").style.display="flex"
+}
+
+function closeDisclaimer(){
+document.getElementById("popup").style.display="none"
+}
